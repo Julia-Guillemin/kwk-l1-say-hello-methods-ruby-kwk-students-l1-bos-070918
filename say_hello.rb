75c7bf5fb3f1718f5815df38de69ed208e690091
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-10 15:30:39 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build your say_hello method here
 
 def say_hello(your_name)
-  say_hello ("charly")
-say_hello ("Randi")
-say_hello ("gabriella")
+say_hello "charly"
+say_hello "Randi"
+say_hello "gabriella"
   puts "say_hello #{your_name}"
 end
 
