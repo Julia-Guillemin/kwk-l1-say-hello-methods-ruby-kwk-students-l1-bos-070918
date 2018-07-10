42b6ab96d50a44fd15186fdecc1cd1b5cb2670b8
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-10 15:25:20 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,9 @@
 # Build your say_hello method here
 
 $your_name=$julia
-def say_hello julia
-  puts "helow #{$julia}"
+def say_hello(julia)
+  puts "helow #{$your_name}"
 end
 
-say_hello
+say_hello ("charly")
+say_hello ("Randi")
